7bb680166163c610d4d7ba024f55be24f1b74c43
sollyu <sollyu@qq.com>
2019-10-17 14:40:46 +0800

    :arrow_up: 升级到AndroidX

--- app/src/main/java/cn/maizz/kotlin/extension/android/sample/MainActivity.kt
+++ app/src/main/java/cn/maizz/kotlin/extension/android/sample/MainActivity.kt
@@ -18,7 +18,7 @@
 package cn.maizz.kotlin.extension.android.sample
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
